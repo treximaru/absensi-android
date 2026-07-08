3c4972ed2fcbb0dd718b5c7f3b955ff61fdf4f58
treximaru <treximaru@gmail.com>
2026-07-08 13:18:04 +0000

    Absensi Android WebView app - absen.berkahsablon.com

---
+++ app/proguard-rules.pro
@@ -0,0 +1,1 @@
+# No special rules needed for WebView app
---
+++ app/src/main/java/com/berkahsablon/absensi/R.java
@@ -0,0 +1,17 @@
+package com.berkahsablon.absensi;
+
+public final class R {
+    public static final class layout {
+        public static final int activity_main = 0x7f040001;
+    }
+    public static final class id {
+        public static final int progressBar = 0x7f080001;
+        public static final int webView = 0x7f080002;
+    }
+    public static final class mipmap {
+        public static final int ic_launcher = 0x7f0c0001;
+    }
+    public static final class string {
+        public static final int app_name = 0x7f0b0001;
+    }
+}
